cb2bd66cb554e24a99a2c345e3b3c71dfefe9524
Nicole Fergestrom <139975090+nfergestrom@users.noreply.github.com>
2025-05-05 10:30:49 -0400

    add open_sink & close_sink functions

---
+++ R/13_open_sink.R
@@ -0,0 +1,14 @@
+#' open sink txt file to store console output in the console_output.txt
+#'
+#' @returns a txt file named console_output.txt
+#' @export
+#'
+open_sink <- function(){
+  
+  # Close sink file if it is open 
+  closeAllConnections()
+  
+  sink("console_output.txt", split = TRUE)
+  sink(stdout(), type = c("output", "message"), split = TRUE)
+  sink(stderr(), type = c("output", "message"), split = TRUE)
+}
