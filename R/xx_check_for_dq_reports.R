21a3b9a438492b871892c6123a3c0eed3c8f4cc5
Andrew Vancil <208506537+avancil-corevitas@users.noreply.github.com>
2025-07-16 11:35:47 -0400

    nvm use importFrom not explicit for most dependencies

--- R/xx_check_for_dq_reports.R
+++ R/xx_check_for_dq_reports.R
@@ -10,8 +10,9 @@
 #' @return A .csv table recording which registries have completed DQ reports for the current month, saved in the specified output location
 #' @export
 #' 
-#' @importFrom dplyr rowwise mutate case_when ungroup group_by select
-#' @importFrom stringr str_detect str_extract
+#' @importFrom dplyr mutate ungroup group_by select left_join summarise
+#' @importFrom stringr str_sub str_starts
+#' @importFrom glue glue
 #' 
 
 
@@ -35,7 +36,7 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
   if (!is.null(year_month)){
     year_month <- year_month
   } else {
-    year_month <- glue::glue(current_year, "-", current_month)
+    year_month <- glue(current_year, "-", current_month)
   }
   
   dq_reps <- tibble::tibble(
@@ -44,7 +45,7 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
                  pattern = year_month,
                  recursive = TRUE)
   ) |> 
-    dplyr::mutate(reg_present = dirname(dirname(dirname(dq_reports))))
+    mutate(reg_present = dirname(dirname(dirname(dq_reports))))
   
   reg_tbl <- tibble::tibble("registries_to_check" = list.files(dq_folder),
                     "lead" = c("Wendi Malley",
@@ -59,23 +60,18 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
                                "Alina Onofrei"))
   
   check_excel_table <- reg_tbl |> 
-    dplyr::left_join(dq_reps, by = c("registries_to_check" = "reg_present")) |> 
-    dplyr::mutate(dq_reports = tidyr::replace_na(dq_reports, "missing"))
-  
-  # Test code below to incorporate base_report_url and output_url? I'm not sure if I'm doing this correctly
-  # check_dq_html <- function(base_html_report_url, output_url) {
-  #   # Checking HTML reports exist in each registry folder on the Biostat Data Files SharePoint site each month
-  #   dir.exists(base_report_url)
+    left_join(dq_reps, by = c("registries_to_check" = "reg_present")) |> 
+    mutate(dq_reports = tidyr::replace_na(dq_reports, "missing"))
     
     # Listing out the html report directories
-    AA_dir <- glue::glue("{base_html_report_url}/AA/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
-    AD_dir <- glue::glue("{base_html_report_url}/AD/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
-    IBD_dir <- glue::glue("{base_html_report_url}/IBD/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
-    MS_dir <- glue::glue("{base_html_report_url}/MS/DQ Checks/Reports/ms/{current_year}/{current_year}-{current_month}/")
-    NMO_dir <- glue::glue("{base_html_report_url}/NMO/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
-    PSO_dir <- glue::glue("{base_html_report_url}/PSO/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
-    RA_dir <- glue::glue("{base_html_report_url}/RA/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
-    RAJ_dir <- glue::glue("{base_html_report_url}/RA Japan/DQ Checks/Reports/raj/{current_year}/{current_year}-{current_month}/")
+    AA_dir <- glue("{base_html_report_url}/AA/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
+    AD_dir <- glue("{base_html_report_url}/AD/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
+    IBD_dir <- glue("{base_html_report_url}/IBD/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
+    MS_dir <- glue("{base_html_report_url}/MS/DQ Checks/Reports/ms/{current_year}/{current_year}-{current_month}/")
+    NMO_dir <- glue("{base_html_report_url}/NMO/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
+    PSO_dir <- glue("{base_html_report_url}/PSO/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
+    RA_dir <- glue("{base_html_report_url}/RA/DQ Checks/Reports/{current_year}/{current_year}-{current_month}/")
+    RAJ_dir <- glue("{base_html_report_url}/RA Japan/DQ Checks/Reports/raj/{current_year}/{current_year}-{current_month}/")
     
     # Vector of registry abbreviations
     reg_list <- c("AA", "AD", "IBD", "MS", "NMO", "PSO", "RA", "RAJ")
@@ -94,9 +90,6 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
     
     # Name the list elements for easier access
     names(dq_html_list) <- paste0("dq_html_reports_", tolower(reg_list))
-    
-  #   return(dq_html_list)
-  # }
   
   # Generate tables for each registry
   dq_html_list <- lapply(reg_dir, function(directory) {
@@ -107,11 +100,11 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
                                    recursive = TRUE))
     # Edit the registry column values for MS and RA Japan to work around them having an extra folder unlike the other registries
     dq_df <- dq_df |>
-      dplyr::mutate(registry = dplyr::if_else(stringr::str_starts(dq_html_reports, "ms"), "MS", registry)) |>
-      dplyr::mutate(registry = dplyr::if_else(stringr::str_starts(dq_html_reports, "raj"), "RAJ", registry)) |>
-      dplyr::group_by(registry) |>
-      dplyr::summarise(dq_html_reports = paste(dq_html_reports, collapse = ", ")) |> # Separate the html reports by a comma if there are multiple per registry
-      dplyr::ungroup()
+      mutate(registry = dplyr::if_else(str_starts(dq_html_reports, "ms"), "MS", registry)) |>
+      mutate(registry = dplyr::if_else(str_starts(dq_html_reports, "raj"), "RAJ", registry)) |>
+      group_by(registry) |>
+      summarise(dq_html_reports = paste(dq_html_reports, collapse = ", ")) |> # Separate the html reports by a comma if there are multiple per registry
+      ungroup()
   })
   
   # Combine all html into one table
@@ -119,8 +112,8 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
   
   #combine html and excel report status tables
   all_reports <- check_excel_table |> 
-    dplyr::left_join(combined_dq_html, by = c("registries_to_check" = "registry")) |> 
-    dplyr::mutate(dq_html_reports = tidyr::replace_na(dq_html_reports, "missing"))
+    left_join(combined_dq_html, by = c("registries_to_check" = "registry")) |> 
+    mutate(dq_html_reports = tidyr::replace_na(dq_html_reports, "missing"))
   
   write.csv(all_reports,
             file = glue::glue("{output_url}dq_report_status_{year_month}.csv"))
