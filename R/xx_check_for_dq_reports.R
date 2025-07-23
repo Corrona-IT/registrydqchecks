c7ee4ca62c257edf6559f555549709ecf806cbe2
Celina Li <cli@corevitas.com>
2025-07-23 17:16:50 -0400

    xx_check - add test code

--- R/xx_check_for_dq_reports.R
+++ R/xx_check_for_dq_reports.R
@@ -18,7 +18,14 @@
 library(tidyverse)
 library(glue)
 library(stringr)
+library(lubridate)
 
+base_report_url <- "C:/Users/lina.li2/PPD (CRG)/Core_Biostat and Epi Team Site - Biostat Registry Data Quality Reports"
+base_html_report_url <- "C:/Users/lina.li2/PPD (CRG)/Biostat Data Files - Registry Data"
+output_url <- "C:/Users/lina.li2/OneDrive - Thermo Fisher Scientific/Documents/DQ Program WG"
+# output_url <- "C:/Users/lina.li2/PPD (CRG)/Core_Biostat and Epi Team Site - Biostat Registries Data Quality Program"
+
+# Define the function check_for_dq_reports
 check_for_dq_reports <- function(base_report_url, base_html_report_url, output_url, year_month = NULL)
 {
   
@@ -36,10 +43,12 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
   
   current_year <- lubridate::year(lubridate::today())
   
+  year_month <- glue::glue("{current_year}-{current_month}")
+  
   if (!is.null(year_month)){
     year_month <- year_month
   } else {
-    year_month <- glue(current_year, "-", current_month)
+    year_month <- glue::glue("{current_year}-{current_month}")
   }
   
   dq_reps <- tibble::tibble(
@@ -93,25 +102,26 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
   
   # Name the list elements for easier access
   names(dq_html_list) <- paste0("dq_html_reports_", tolower(reg_list))
-  
-  # Generate tables for each registry
-  dq_html_list <- lapply(reg_dir, function(directory) {
-    dq_df <- tibble::tibble(
-      registry = basename(dirname(dirname(dirname(dirname(directory))))),
-      dq_html_reports = list.files(directory,
-                                   pattern = ".html",
-                                   recursive = TRUE))
-    # Edit the registry column values for MS and RA Japan to work around them having an extra folder unlike the other registries
-    dq_df <- dq_df |>
-      mutate(registry = dplyr::if_else(str_starts(dq_html_reports, "ms"), "MS", registry)) |>
-      mutate(registry = dplyr::if_else(str_starts(dq_html_reports, "raj"), "RAJ", registry)) |>
-      group_by(registry) |>
-      summarise(dq_html_reports = paste(dq_html_reports, collapse = ", ")) |> # Separate the html reports by a comma if there are multiple per registry
-      ungroup()
-  })
+    
+    # Define the function to process each directory
+  dq_html_list <- function(directory) {
+      tibble::tibble(
+        registry = basename(dirname(dirname(dirname(dirname(directory))))),
+        dq_html_reports = list.files(directory, pattern = ".html", recursive = TRUE)
+      ) %>%
+      # Edit the registry column values for MS and RA Japan to work around them having an extra folder unlike the other registries
+        mutate(registry = dplyr::if_else(str_starts(dq_html_reports, "ms"), "MS", registry)) %>%
+        mutate(registry = dplyr::if_else(str_starts(dq_html_reports, "raj"), "RAJ", registry)) %>%
+        group_by(registry) %>%
+        summarise(dq_html_reports = paste(dq_html_reports, collapse = ", ")) %>% # Separate the html reports by a comma if there are multiple per registry
+        ungroup()
+    }
+    
+    # Use the defined function within lapply
+   dq_df <- lapply(reg_dir, dq_html_list)
   
   # Combine all html into one table
-  combined_dq_html <- dplyr::bind_rows(dq_html_list)
+  combined_dq_html <- dplyr::bind_rows(dq_df)
   
   #combine html and excel report status tables
   all_reports <- check_excel_table |> 
@@ -124,8 +134,14 @@ check_for_dq_reports <- function(base_report_url, base_html_report_url, output_u
 }
 
 # Testing function
+# base_report_url <- "C:/Users/lina.li2/PPD (CRG)/Biostat Data Files - Registry Data"
+output_url <- "C:/Users/lina.li2/OneDrive - Thermo Fisher Scientific/Documents/DQ Program WG"
+dir.exists(output_url)
 
 test <- check_for_dq_reports(base_report_url = "C:/Users/lina.li2/PPD (CRG)/Core_Biostat and Epi Team Site - Biostat Registry Data Quality Reports",
-                                 base_html_report_url = "C:/Users/lina.li2/PPD (CRG)/Biostat Data Files - RA Japan/DQ checks/Reports/raj", 
-                                 output_url = "C:/Users/lina.li2/PPD (CRG)/Core_Biostat and Epi Team Site - Biostat Registries Data Quality Program", 
-                                 year_month = "2025-07")
+                             base_html_report_url = "C:/Users/lina.li2/PPD (CRG)/Biostat Data Files - Registry Data",
+                             output_url = "C:/Users/lina.li2/OneDrive - Thermo Fisher Scientific/Documents/DQ Program WG",
+                             year_month = "2025-07")
+
+test <- check_for_dq_reports(base_report_url, base_html_report_url, output_url, year_month = "2025-07")
+
