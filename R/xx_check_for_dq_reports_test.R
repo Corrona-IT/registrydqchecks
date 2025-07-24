9fb843a11e61b774094ba5bb7128ef54698afb29
Celina Li <cli@corevitas.com>
2025-07-24 14:03:37 -0400

    add setwd (had to change wd to run this test)

--- R/xx_check_for_dq_reports_test.R
+++ R/xx_check_for_dq_reports_test.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(stringr)
 library(glue)
 
+setwd("C:/Users/lina.li2/OneDrive - Thermo Fisher Scientific/Documents/GitHub/registrydqchecks/R")
+
 base_report_url <- "C:/Users/andrew.vancil/PPD (CRG)/Core_Biostat and Epi Team Site - Biostat Registry Data Quality Reports/"
 base_html_report_url <- "C:/Users/andrew.vancil/PPD (CRG)/Biostat Data Files - Registry Data/"
 output_url <- "C:/Users/andrew.vancil/OneDrive - Thermo Fisher Scientific/Documents/tests/"
@@ -11,9 +13,9 @@ base_html_report_url <- "C:/Users/lina.li2/PPD (CRG)/Biostat Data Files - Regist
 output_url <- "C:/Users/lina.li2/OneDrive - Thermo Fisher Scientific/Documents/DQ Program WG/"
 # output_url <- "C:/Users/lina.li2/PPD (CRG)/Core_Biostat and Epi Team Site - Biostat Registries Data Quality Program"
 
-
 source("xx_check_for_dq_reports.R")
 
+# Call function check_for_dq_reports and assign a year_month date of interest 
 check_for_dq_reports(
   base_report_url,
   base_html_report_url,
